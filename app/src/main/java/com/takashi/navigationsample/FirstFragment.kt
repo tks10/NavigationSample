9f8cdd723540e05bbd66ab6936d549b35657ea94
tks10 <tktktks10@gmail.com>
2018-09-14 16:02:31 +0900

    add: Third Fragment

--- app/src/main/java/com/takashi/navigationsample/FirstFragment.kt
+++ app/src/main/java/com/takashi/navigationsample/FirstFragment.kt
@@ -8,6 +8,6 @@ import android.view.ViewGroup
 
 class FirstFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_first, container, false)
     }
 }
--- app/src/main/java/com/takashi/navigationsample/MainActivity.kt
+++ app/src/main/java/com/takashi/navigationsample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.takashi.navigationsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onSupportNavigateUp()
+            = findNavController(R.id.nav_host_fragment).navigateUp()
 }
