c8265089dbb73ff225a0e7f277943799249342f8
tks10 <tktktks10@gmail.com>
2018-09-14 15:21:48 +0900

    add: First Fragment

---
+++ app/src/main/java/com/takashi/navigationsample/FirstFragment.kt
@@ -0,0 +1,13 @@
+package com.takashi.navigationsample
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
