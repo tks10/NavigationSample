401ce71a9fc2bc55fa8841b45841defb8f1aebc1
tks10 <tktktks10@gmail.com>
2018-09-14 17:15:36 +0900

    update: done transit

--- app/src/main/java/com/takashi/navigationsample/SecondFragment.kt
+++ app/src/main/java/com/takashi/navigationsample/SecondFragment.kt
@@ -5,9 +5,16 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_second.view.*
 
 class SecondFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+
+        view.button?.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_second_to_third)
+        }
+        return view
     }
 }
--- app/src/main/java/com/takashi/navigationsample/ThirdFragment.kt
+++ app/src/main/java/com/takashi/navigationsample/ThirdFragment.kt
@@ -5,9 +5,16 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_third.view.*
 
 class ThirdFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_third, container, false)
+        val view = inflater.inflate(R.layout.fragment_third, container, false)
+
+        view.button?.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_third_to_first)
+        }
+        return view
     }
 }
