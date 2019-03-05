0dc5e7b999a08e9386d660bb390a42dbebbbb2ac
tks10 <tktktks10@gmail.com>
2019-03-05 15:17:26 +0900

    update

--- app/src/main/java/com/takashi/navigationsample/FirstFragment.kt
+++ app/src/main/java/com/takashi/navigationsample/FirstFragment.kt
@@ -11,7 +11,6 @@ import kotlinx.android.synthetic.main.fragment_first.view.*
 class FirstFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_first, container, false)
-
         view.button?.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_first_to_second)
         }
